a74c130302158f182b1cf60e6fa47470925588c5
bastienFouquet <bastien.fouquet85@gmail.com>
2020-12-15 16:58:27 +0100

    Person and PersonType models linked

--- Ehpad.ORM/PersonType.cs
+++ Ehpad.ORM/PersonType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Ehpad.ORM
@@ -7,5 +8,11 @@ namespace Ehpad.ORM
     public class PersonType
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Label { get; set; }
+
+        public List<Person> Persons { get; } = new List<Person>();
     }
 }
